b4fb33a1097549175208fe21d70d067885646d32
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-06-30 10:39:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 
-const Promise = require('bluebird');
-  Promise.config({
-    cancellation: true
-  });
-
 const port = process.env.PORT;
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -19,18 +14,10 @@ app.use(cors());
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
-
-  if(bot.isPolling()) {
-    await bot.stopPolling();
-    }
-    await bot.startPolling();
-
-
   await bot.sendMessage(
     chatId,
     `${msg.text}
     Вітаю ${msg.from.first_name}! Все що я зараз можу, це повторювати за тобою, напиши команду /start і роби замовлення`)
-
     if(text === '/start'){
     await bot.sendMessage(chatId, 'Ласкаво просимо', {
       reply_markup: {
@@ -40,7 +27,6 @@ bot.on("message", async (msg) => {
       }
   })
   }
-  await bot.stopPolling();
 });
 
 app.listen(port, () => console.log('server started on port:' + port))
